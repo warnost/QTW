7fb83e82e05df1e4e626e27b4e5157155f79b418
warnost <warnost@gmail.com>
2020-09-20 23:32:41 -0400

    density and results

--- Will/cherry/animate.R
+++ Will/cherry/animate.R
@@ -9,20 +9,16 @@ plt.data <- combined[complete.cases(combined),]
 medians <- plt.data %>% select(year,age) %>% group_by(year) %>% summarise(median.age=median(age))
 medians$text <- paste(medians$year, "Median Age:",medians$median.age)
 
-## I tried to make my own color palette. I failed.
-library(RColorBrewer)
-colourCount = length(unique(plt.data$year))
-getPalette = colorRampPalette(brewer.pal(9, "Set1"))
-
-
 only1999 <- plt.data %>% filter(year == 1999) %>% select(age)
+med99 <-medians%>% filter(year == 1999) %>% select(median.age, text)
 
 ## ggplot object
 anim<-ggplot(plt.data) + geom_density(aes(x=age, fill=as.factor(year))) + 
   geom_density(data = only1999, aes(x=age)) +
-  geom_vline(data=medians, aes(xintercept=median.age),
-             linetype="dashed") +
+  geom_vline(data=medians, aes(xintercept=median.age), linetype="dashed") +
+  geom_vline(data=med99, aes(xintercept=median.age), linetype="dashed") +
   geom_text(data=medians, mapping=aes(x=median.age, y=0, label=text), size=4, angle=90, vjust=-0.4, hjust=0) +
+  geom_text(data=med99, mapping=aes(x=median.age, y=0, label=text), size=4, angle=90, vjust=1.1, hjust=0) +
   theme(panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(),
         panel.background = element_blank(),
@@ -30,12 +26,12 @@ anim<-ggplot(plt.data) + geom_density(aes(x=age, fill=as.factor(year))) +
         plot.subtitle = element_text(size=16),
         legend.title = element_blank(),
         axis.line = element_line(colour = "black")) +
-  scale_fill_manual(values=wes_palette(n=14, name="FantasticFox1", type = "continuous")) + 
+  scale_fill_manual(values=wes_palette(n=15, name="FantasticFox1", type = "continuous")) + 
   transition_time(year) +
   labs(title = "Age Distribution of Cherry Blossom Runners (1999 - 2012)", subtitle = "Year: {frame_time}")
 
 ## animate it
 anim2 <- animate(anim, duration = 40, end_pause = 20, height = 400, width = 600)
-
+anim2
 ## Save the gif
 anim_save(file='density_v3.gif',animation = anim2)
